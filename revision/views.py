b9b82140c02626f1ee181fbb9726609d5d50b829
Nauman <shaikhnomu2002@gmail.com>
2025-07-09 16:03:52 +0530

    ✅ Day 8: Completed topic_list view for Revision app

--- revision/views.py
+++ revision/views.py
@@ -5,3 +5,22 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import paginator
 from django.contrib import messages
 
+@login_required
+def topic_list(request):
+    if request.method=="POST":
+        form=TopicForm(request.POST)
+        if form.is_valid():
+            topic=form.save(commit=False)
+            topic.user=request.user
+            topic.save()
+            messages.success(request,'Topic added successfully!')
+            return redirect ('topic_list')
+    else:
+        form=TopicForm()
+    
+    topics=Topic.objects.filter(user=request.user).order_by('-created_at')
+    paginator=Paginator(topics,5)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+
+    return render(request,'revision/topic_list.html',{'form':form,'page_obj':page_obj})
